dbe5223648522708810fa109b463511202999b38
Anton Kuptsov <steambot33@gmail.com>
2021-07-21 18:19:02 +0300

    [feat] add Hero

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Head } from "../sections/Head";
 import { AppBar } from "../sections/AppBar";
 import { Footer } from "../sections/Footer";
+import { Hero } from "../sections/Hero";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head />
       <AppBar />
-      <main className={styles.main}></main>
+
+      <main className={styles.main}>
+        <Hero />
+      </main>
+
       <Footer />
     </div>
   );
--- sections/Hero/Hero.tsx
+++ sections/Hero/Hero.tsx
@@ -2,8 +2,32 @@ import style from './Hero.module.css';
 
 export const Hero = () => {
     return (
-        <div className={style.container}>
-
-        </div>
+        <section className={style.container}>
+            <div className={style.wrapper}>
+                <iframe
+                    src="https://player.vimeo.com/video/535592308?autoplay=1&muted=1&loop=1&byline=0&portrait=0"
+                    frameBorder="0"
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    allowFullScreen
+                    id="video"
+                />
+                <div className={style.muteButton} id="unmute">
+                    <div>Включить звук</div>
+                </div>
+                <script src="https://player.vimeo.com/api/player.js" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        const btn = document.getElementById('unmute');
+                        btn.addEventListener("click", () => {
+                            var player = new Vimeo.Player("video");
+                            player.setMuted(false);
+                            btn.style.display='none';
+                            }
+                            );`
+                    }}
+                />
+            </div>
+        </section>
     );
 }
