3d522c82cb7343627a6150638f49d5a5d44114a3
Anton Kuptsov <steambot33@gmail.com>
2021-09-14 20:10:56 +0300

    [feat](ENG)(Poster) add text animation with react-reveal

--- sections/Poster/Story.jsx
+++ sections/Poster/Story.jsx
@@ -1,21 +1,21 @@
 import style from "./Poster.module.css";
 import { menuLinks } from "../../app.config";
+import Fade from "react-reveal/Fade";
 export const Story = () => {
   return (
     <>
       <div className={style.text} id={menuLinks.STORY}>
         <div className={style.title}>A Story</div>
-        <div
-        //   className={inView && style.animation}
-        >
+
+        <Fade right>
           <p>
             <strong>PAPA SRAPA</strong> is not a typical music documentary, but
             rather a condensed trip to the very heart of Noise. It is loaded
             with music that can hurt and heal, Russian weird avant-garde and
             savage vigor of its preeminent performer.
           </p>
-        </div>
-        <div>
+        </Fade>
+        <Fade right>
           <p>
             The film takes us to bizarre noise festivals and performances with
             ravers, tripsters and freaks immersing into worlds of sonic violence
@@ -23,8 +23,8 @@ export const Story = () => {
             to 1920-s, binding it to early Soviet avant-garde figures, such as
             Dziga Vertov, Arseniy Avraamov and Nikolai Kulbin.
           </p>
-        </div>
-        <div>
+        </Fade>
+        <Fade right>
           <p>
             The craftsmаn should make his own tools - so Papa makes his of old
             Soviet radio components and cheap junk. We show it is not just about
@@ -32,7 +32,7 @@ export const Story = () => {
             brings them to life. His weapons, known to the best part of Russian
             electronic musicians.
           </p>
-        </div>
+        </Fade>
       </div>
     </>
   );
