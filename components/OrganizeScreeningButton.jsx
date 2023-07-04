7f45ad12a037feef41980f11ad7e1745e58e8d48
Anton Kuptsov <39290991+anton-kuptsov@users.noreply.github.com>
2023-07-04 08:41:28 +0300

    refactor: wrap OrganizeScreeningButton with next/link

--- components/OrganizeScreeningButton.jsx
+++ components/OrganizeScreeningButton.jsx
@@ -1,22 +1,8 @@
 import { menuLinks } from "../constants/menuLinks";
+import Link from "next/link";
 
 export const OrganizeScreeningButton = () => (
-  <a
-    href={`/${menuLinks.CONTACT}`}
-    style={{
-      color: "white",
-      border: "1px solid white",
-      padding: "16px",
-      borderRadius: "5px",
-      textDecoration: "none",
-      display: "block",
-      margin: "100px auto",
-      fontWeight: "bold",
-      fontSize: "1.2rem",
-      backgroundColor: "rgba(20, 110, 20, 0.7)",
-      boxShadow: "0 0 10px 5px rgba(20, 110, 20, 0.80)",
-    }}
-  >
-    Organize a screening in your city
-  </a>
+  <Link href={`/${menuLinks.CONTACT}`} passHref>
+    <a className="organize-button">Organize a screening in your city</a>
+  </Link>
 );
