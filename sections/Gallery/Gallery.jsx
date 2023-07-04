52d47c9c055e90e448ae835bfc01a5d5b78efae1
Anton Kuptsov <39290991+anton-kuptsov@users.noreply.github.com>
2023-07-04 12:38:57 +0300

    refactor: optimize gallery

--- sections/Gallery/Gallery.jsx
+++ sections/Gallery/Gallery.jsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
-import style from "./Gallery.module.css";
+import { useCallback, useState } from "react";
 import { images } from "./data";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { menuLinks } from "../../constants/menuLinks";
+import img0 from "../../public/gallery/1.jpg";
+import img1 from "../../public/gallery/2.jpg";
+import img2 from "../../public/gallery/3.jpg";
+import img3 from "../../public/gallery/4.jpg";
+import img4 from "../../public/gallery/5.jpg";
+import img5 from "../../public/gallery/6.jpg";
+import img6 from "../../public/gallery/7.jpg";
+import img7 from "../../public/gallery/8.jpg";
+import img8 from "../../public/gallery/9.jpg";
+import img9 from "../../public/gallery/10.jpg";
+import img10 from "../../public/gallery/11.jpg";
+import img11 from "../../public/gallery/12.jpg";
+import img12 from "../../public/gallery/13.jpg";
 
 export const Gallery = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -18,150 +30,135 @@ export const Gallery = () => {
     setViewerIsOpen(false);
   };
 
+  const smallImgClasses =
+    "object-cover object-center aspect-square max-w-[50%] hover:cursor-pointer";
+  const midImgWrapClasses = "aspect-square max-w-[33.33%] w-full relative";
+  const midImgClasses =
+    "object-cover object-center aspect-square hover:cursor-pointer";
+
   return (
-    <div className={style.container} id={menuLinks.GALLERY}>
-      <div className={style.wrapper}>
-        <Image
-          src={images[0].src}
-          width={650}
-          height={650}
-          objectFit="cover"
-          objectPosition="center"
-          quality={100}
-          unoptimized
-          alt={images[0].caption}
-          onClick={() => openLightbox(0)}
-        />
-        <div className={style.grid_4}>
+    <div
+      className="w-full bg-black max-w-full overflow-hidden flex flex-col gap-2.5"
+      id={menuLinks.GALLERY}
+    >
+      <div className="flex gap-2.5">
+        <div className="w-full max-w-[50%] aspect-square relative">
           <Image
-            src={images[1].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[1].caption}
-            onClick={() => openLightbox(1)}
+            src={img0}
+            className="object-cover hover:cursor-pointer"
+            fill
+            quality={100}
             unoptimized
+            alt="PapaSrapa backstage"
+            onClick={() => openLightbox(0)}
           />
-          <Image
-            src={images[2].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[2].caption}
-            onClick={() => openLightbox(2)}
-            unoptimized
-          />
-          <Image
-            src={images[3].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[3].caption}
-            onClick={() => openLightbox(3)}
-            unoptimized
-          />
-          <Image
-            src={images[4].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[4].caption}
-            onClick={() => openLightbox(4)}
-            unoptimized
-          />
+        </div>
+        <div className="flex flex-col max-w-[50%] gap-2.5">
+          <div className="flex gap-2.5">
+            <Image
+              src={img1}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(1)}
+            />
+            <Image
+              src={img2}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(2)}
+            />
+          </div>
+          <div className="flex gap-2.5">
+            <Image
+              src={img3}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(3)}
+            />
+            <Image
+              src={img4}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(4)}
+            />
+          </div>
         </div>
       </div>
 
-      <div className={style.wrapper}>
-        <div className={style.grid_4}>
+      <div className="flex gap-2.5">
+        <div className="flex flex-col max-w-[50%] gap-2.5 overflow-hidden">
+          <div className="flex gap-2.5">
+            <Image
+              src={img5}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(5)}
+            />
+            <Image
+              src={img6}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(6)}
+            />
+          </div>
+          <div className="flex gap-2.5">
+            <Image
+              src={img7}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(7)}
+            />
+            <Image
+              src={img8}
+              className={smallImgClasses}
+              alt="PapaSrapa backstage"
+              onClick={() => openLightbox(8)}
+            />
+          </div>
+        </div>
+        <div className="w-full max-w-[50%] aspect-square relative">
           <Image
-            src={images[5].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[5].caption}
-            onClick={() => openLightbox(5)}
+            src={img9}
+            className="object-cover hover:cursor-pointer"
+            fill
+            quality={100}
             unoptimized
-          />
-          <Image
-            src={images[6].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[6].caption}
-            onClick={() => openLightbox(6)}
-            unoptimized
-          />
-          <Image
-            src={images[7].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[7].caption}
-            onClick={() => openLightbox(7)}
-            unoptimized
-          />
-          <Image
-            src={images[8].src}
-            width={650}
-            height={650}
-            objectFit="cover"
-            objectPosition="center"
-            alt={images[8].caption}
-            onClick={() => openLightbox(8)}
-            unoptimized
+            alt="PapaSrapa backstage"
+            onClick={() => openLightbox(9)}
           />
         </div>
-        <Image
-          src={images[9].src}
-          width={650}
-          height={650}
-          objectFit="cover"
-          objectPosition="center"
-          alt={images[9].caption}
-          onClick={() => openLightbox(9)}
-          unoptimized
-        />
       </div>
-
-      <div className={style.grid_3}>
-        <Image
-          src={images[10].src}
-          width={650}
-          height={650}
-          objectFit="cover"
-          objectPosition="center"
-          alt={images[10].caption}
-          onClick={() => openLightbox(10)}
-          unoptimized
-        />
-        <Image
-          src={images[11].src}
-          width={650}
-          height={650}
-          objectFit="cover"
-          objectPosition="center"
-          alt={images[11].caption}
-          onClick={() => openLightbox(11)}
-          unoptimized
-        />
-        <Image
-          src={images[12].src}
-          width={650}
-          height={650}
-          objectFit="cover"
-          objectPosition="center"
-          alt={images[12].caption}
-          onClick={() => openLightbox(12)}
-          unoptimized
-        />
+      <div className="flex gap-2.5 w-full">
+        <div className={midImgWrapClasses}>
+          <Image
+            src={img10}
+            className={midImgClasses}
+            fill
+            quality={100}
+            alt="PapaSrapa backstage"
+            onClick={() => openLightbox(10)}
+          />
+        </div>
+        <div className={midImgWrapClasses}>
+          <Image
+            src={img11}
+            className={midImgClasses}
+            fill
+            quality={100}
+            alt="PapaSrapa backstage"
+            onClick={() => openLightbox(11)}
+          />
+        </div>
+        <div className={midImgWrapClasses}>
+          <Image
+            src={img12}
+            className={midImgClasses}
+            fill
+            quality={100}
+            alt="PapaSrapa backstage"
+            onClick={() => openLightbox(12)}
+          />
+        </div>
       </div>
 
       <ModalGateway>
@@ -171,7 +168,6 @@ export const Gallery = () => {
               currentIndex={currentImage}
               views={images.map((item) => ({
                 ...item,
-                source: item.src,
               }))}
             />
           </Modal>
