282b9411ebe453afdb91ff301cc241f3fe9cff50
Nikita Kabardin <nikita@kabardin.com>
2025-09-23 10:57:46 +0200

    fix build ?

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  output: 'standalone',
+  // Exclude directories with spaces in their names from the build process
+  webpack: (config, { isServer }) => {
+    // Exclude the "screenings map" directory
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules/**', '**/screenings map/**']
+    };
+    return config;
+  },
 };
