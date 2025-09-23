3b968fda90d20a7535b24d454f590bebf6931e78
Nikita Kabardin <nikita@kabardin.com>
2025-09-23 11:07:27 +0200

    try to fix build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   reactStrictMode: true,
   output: 'standalone',
-  outputFileTracingIgnores: {
-    '*': ['**/screenings map/**']
+  // Exclude directories with spaces in their names from the build process
+  webpack: (config) => {
+    // Exclude the "screenings map" directory from webpack processing
+    if (config.module && config.module.rules) {
+      config.module.rules.push({
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: [/node_modules/, /screenings map/],
+        use: []
+      });
+    }
+    return config;
   },
-  webpack: (config) => config, // no watchOptions in CI
+  // Exclude the "screenings map" directory from file tracing
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['**/screenings map/**', '**/node_modules/**']
+    }
+  }
 };
